0aebc54240cc064dbc5d7fd19d6b1cc18848c9df
aglab2 <aglab3@gmail.com>
2024-05-11 17:51:56 +0800

    Added area aware panels fix

--- panels-fix.asm
+++ panels-fix.asm
@@ -1,27 +1,51 @@
-.orga 0x632D0
-    LUI A0, 0x1300
-    JAL 0x80277F50
-    ORI A0, A0, 0x05D8
-
-objects_loop:
-    LUI T1, 0x8036
-    ORI T1, T1, 0x0E88
-    LW V1, 0x18(SP)
-    BEQ T1, V1, 0x63314
-
-    LW T2, 0x20C(V1)
-    BNE T2, V0, wrong_object
-    NOP
+.orga 0x632bc
+	ADDIU SP, SP, 0xFFE8
+	SW RA, 0x0014 (SP)
 	
-	LW T4, 0x0F0(V1)
+	LUI T9, 0x8036
+	LW T9, 0x1160 (T9)
+	LW T9, 0x14C(T9)
+	BNE T9, R0, end
 
-.orga 0x63304
-wrong_object:
-    ADDIU V1, V1, 0x260
-    SW V1, 0x18(SP)
-    B objects_loop
-    NOP
+	LUI AT, 0x1300
+	JAL 0x00277F50
+	ORI A0, AT, 0x05D8
+	LUI AT, 0x8034
+	ADDIU AT, AT, 0xCBE0
+	LW V1, 0x0408 (AT)
+	ADDIU A0, AT, 0x03A8
+	BEQ V1, A0, spawn_star
+	NOP
 
+loop:
+	LW T8, 0x020C (V1)
+	BNE T8, V0, next
+	LHU T7, 0x0074 (V1)
+	BEQ T7, R0, next
+	LW T6, 0x00F0 (V1)
+	BEQ T6, R0, end
+	
+next:
+	LW V1, 0x0060 (V1)
+	BNE V1, A0, loop
+	NOP
+
+spawn_star:
+	JAL 0x802A8CDC
+	NOP
+	LUI T9, 0x8036
+	LW T9, 0x1160 (T9)
+	SW K0, 0x14C (T9)
+	LWC1 F12, 0x00A0 (T9)
+	LWC1 F14, 0x00A4 (T9)
+	LW A2, 0x00A8 (T9)
+	JAL 0x002F2B88
+	NOP
+	
+end:
+	LW RA, 0x0014 (SP)
+	JR RA
+	ADDIU SP, SP, 0x0018
 
 .orga 0x63CE8
     NOP
